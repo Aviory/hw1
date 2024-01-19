a2bb3ce2dcbf676e02442ed74c97ad457d711a49
Vel <avior@gmail.com>
2024-01-19 19:30:10 +0200

    add hello world

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
+// git init
+//git add .
