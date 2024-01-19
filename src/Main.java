4a400ee3e3446ef01444e44ebe8e6b6b1feed7b4
Vel <avior@gmail.com>
2024-01-19 19:37:26 +0200

    222

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Darth!");
+        System.out.println("Hello Darth222");
 
     }
 
