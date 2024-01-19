82ba50bc7b31be794d60ddf17e40f213fe139fa8
Vel <avior@gmail.com>
2024-01-19 19:34:36 +0200

    change to Darth

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,13 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Darth!");
+
     }
 
 }
 // git init
 //git add .
+//git remote add origin https://github.com/Aviory/hw1.git
+//git commit -m "first commit"
+//git push -u origin master
